81d23607110df42c559a05759f1e807bb24c9cd7
Tanya Vynohradova <tanya.germanuk@gmail.com>
2023-04-12 21:22:14 +0300

    add class StartCommand

---
+++ src/main/java/org/example/StartCommand.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import java.util.Collections;
+
+public class StartCommand extends BotCommand {
+
+    public StartCommand() {
+        super("start", "Start command");
+    }
+
+    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+        String textHello = "Вітаємо вас! Цей бот допоможе відслідкувати актуальні курси валют";
+
+        SendMessage message = new SendMessage();
+        message.setText(textHello);
+        message.setChatId(chat.getId());
+
+        InlineKeyboardButton buttonInfo = InlineKeyboardButton
+                .builder()
+                .text("Отримати інформацію")
+                .callbackData("Отримати інформацію")
+                .build();  // створили кнопку1 під текстом
+        InlineKeyboardButton buttonSettings = InlineKeyboardButton
+                .builder()
+                .text("Налаштування")
+                .callbackData("Налаштування")
+                .build(); //створили кнопку2 під текстом
+        InlineKeyboardMarkup keyboard = InlineKeyboardMarkup
+                .builder()
+                .keyboard(Collections.singleton(Collections.singletonList(buttonInfo)))
+                .keyboard(Collections.singleton(Collections.singletonList(buttonSettings)))
+                .build(); // створили клавіатуру з кнопками
+
+        message.setReplyMarkup(keyboard);
+        try {
+            absSender.execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
